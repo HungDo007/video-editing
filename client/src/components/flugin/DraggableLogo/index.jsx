df99eb6a982b6491102876be7280350cf2bad7d2
TamLH <luongtam962@gmail.com>
2022-07-05 23:55:27 +0700

    upcode

--- client/src/components/flugin/DraggableLogo/index.jsx
+++ client/src/components/flugin/DraggableLogo/index.jsx
@@ -6,77 +6,15 @@ import {
   DialogTitle,
   Grid,
   IconButton,
-  TextField,
 } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import bg from "./default-video-image.png";
-import Draggable from "react-draggable";
-import { useEffect } from "react";
-
-const DialogResize = (props) => {
-  const { w, h, open, handleClose, onSubmit } = props;
-
-  useEffect(() => {
-    setWidth(w);
-    setHeight(h);
-  }, [w, h]);
-  const [width, setWidth] = useState();
-  const [height, setHeight] = useState();
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogContent dividers={true}>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              value={width}
-              label="Width"
-              variant="standard"
-              onChange={(e) => setWidth(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              value={height}
-              label="Height"
-              variant="standard"
-              onChange={(e) => setHeight(e.target.value)}
-            />
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button variant="contained" onClick={() => onSubmit([width, height])}>
-          Save
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
+import { Rnd } from "react-rnd";
 
 function DialogDraggableLogo(props) {
   const { open, handleClose, logo, onTrack, onResize } = props;
-  const [openResize, setOpenResize] = useState(false);
-  const [logoSelected, setLogoSelected] = useState();
 
-  const trackPos = (data, l) => {
-    const oldPos = { ...l.position };
-    const newPos = {
-      x: data.x < -290 || data.x > 800 ? oldPos.x : data.x,
-      y: data.y < 0 || data.y > 350 ? oldPos.y : data.y,
-    };
-    onTrack(l, newPos);
-  };
-
-  const doubleClick = (l) => {
-    setOpenResize(true);
-    setLogoSelected(l);
-  };
-
-  const rehandleResize = (size) => {
-    setOpenResize(false);
-    onResize(logoSelected, size);
-  };
   return (
     <Dialog open={open} fullWidth={true} maxWidth="lg">
       <DialogTitle
@@ -95,13 +33,6 @@ function DialogDraggableLogo(props) {
         </IconButton>
       </DialogTitle>
       <DialogContent dividers={true}>
-        <DialogResize
-          open={openResize}
-          w={logoSelected?.size[0]}
-          h={logoSelected?.size[1]}
-          handleClose={() => setOpenResize(false)}
-          onSubmit={rehandleResize}
-        />
         <Grid container spacing={2}>
           <Grid item xs={3}></Grid>
           <Grid
@@ -117,26 +48,27 @@ function DialogDraggableLogo(props) {
           >
             {logo?.map((l) => {
               return (
-                <Draggable
-                  onDrag={(e, data) => trackPos(data, l)}
-                  position={l.position}
+                <Rnd
+                  size={{ width: l.size[0], height: l.size[1] }}
+                  position={{ x: l.position.x, y: l.position.y }}
+                  onDragStop={(e, d) => {
+                    onTrack(l, { x: d.x, y: d.y });
+                  }}
+                  onResizeStop={(e, direction, ref, delta, position) => {
+                    onResize(l, [ref.style.width, ref.style.height]);
+                  }}
                 >
                   <div
                     style={{
-                      position: "absolute",
-                      cursor: "move",
+                      backgroundImage: `url(${l.file_name})`,
+                      backgroundSize: "100% 100%",
+                      backgroundRepeat: "no-repeat",
+                      width: parseInt(l.size[0]),
+                      height: parseInt(l.size[1]),
                       border: `1px solid ${l.position.x < 0 ? "red" : "green"}`,
                     }}
-                    onDoubleClick={() => doubleClick(l)}
-                  >
-                    <img
-                      src={l.file_name}
-                      alt=""
-                      width={l.size[0]}
-                      height={l.size[1]}
-                    />
-                  </div>
-                </Draggable>
+                  ></div>
+                </Rnd>
               );
             })}
           </Grid>
